0a094db2228055a0465138600803ebbfa79c4399
Marcelooalves <marceloo15200@gmail.com>
2026-05-20 20:35:02 -0300

    Criação da função removerLivro

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Biblioteca } from "./models/Biblioteca.js";
+import { Biblioteca } from "./models/biblioteca.js";
 import { Livro } from "./models/Livro.js";
 import { Usuario } from "./models/Usuario.js";
 import { Funcionario } from "./models/Funcionario.js";
@@ -23,3 +23,11 @@ funcionario.aprovarEmprestimo(usuario, livro1);
 EmprestimoService.realizarEmprestimo(usuario, livro1);
 
 console.log(usuario.listarLivros());
+
+console.log('\n Livros disponiveis antes da remoção: ');
+console.log(biblioteca.livros);
+
+biblioteca.removerLivro(livro1);
+
+console.log('\n Livros disponiveis após a remoção: ');
+console.log(biblioteca.livros);
--- src/models/biblioteca.js
+++ src/models/biblioteca.js
@@ -16,4 +16,14 @@ export class Biblioteca {
     listarLivrosDisponiveis() {
         return this.livros.filter(livro => livro.disponivel);
     }
-}
+
+    removerLivro(livro){
+        const posicao = this.livros.indexOf(livro);
+
+        if (posicao !== -1){
+            this.livros.splice(posicao, 1);
+        }
+        }
+
+
+    }
