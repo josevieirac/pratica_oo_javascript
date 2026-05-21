02f204d38913bda3f77f8d210baf1de126605043
thiagocosta870 <titocosta416@gmail.com>
2026-05-21 12:38:12 -0300

    Implementing Getteres and Setteres

--- src/models/livro.js
+++ src/models/livro.js
@@ -1,11 +1,42 @@
 export class Livro {
     #titulo;
     #autor;
+    #disponivel;
 
     constructor(titulo, autor) {
-        this.#titulo = titulo;
-        this.#autor = autor;
-        this.#disponivel = true;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+    }
+
+    get titulo() {
+        return this.#titulo;
+    }
+
+    set titulo(novoTitulo) {
+        if(!novoTitulo || novoTitulo.trim() === ""){
+            throw new Error ("O livro precisa ter um título. ");
+        }
+        this.#titulo= novoTitulo;
+    }
+
+    get autor() {
+        return this.#autor;
+    }
+
+    set autor(novoAutor) {
+        if (!novoAutor || novoAutor.trim() === "") {
+            throw new Error ("Preencha o nome do autor. ");
+        }
+        this.#autor = novoAutor;
+    }
+
+    get disponivel() {
+        return this.#disponivel;
+    }
+
+    set disponivel(disponivelAtualizado) {
+        this.#disponivel = disponivelAtualizado;
     }
 
     emprestar() {
